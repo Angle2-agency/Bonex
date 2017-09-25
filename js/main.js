ce9e7054dfaf39da086225548718d3c0a3b6245b
Scooter)Ok <scooterok@gmail.com>
2017-09-25 13:37:11 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-	//$.scrollSpeed(200, 700);
-	//$('.frame-3').addClass('animate');
+	$.scrollSpeed(200, 800);
+	//$('.frame-5').addClass('animate');
 	//$('.frame-2 ul').addClass('animate');
 	frameStarts.init();
 	scroller();
@@ -19,10 +19,10 @@ var frameStarts = {
 		frameStarts.points.w1 = $('.frame-2 .w1').offset().top - 500;
 		frameStarts.points.ul1 = $('.frame-2 .ul-1').offset().top - 600;
 		frameStarts.points.ul2 = $('.frame-2 .ul-2').offset().top -500;
-		frameStarts.points.ul3 = $('.frame-2 .ul-3').offset().top -500;
+		frameStarts.points.ul3 = $('.frame-2 .ul-3').offset().top -750;
 		frameStarts.points.frame3 = $('.frame-3').offset().top -h;
-		frameStarts.points.frame4 = $('.frame-4').offset().top -h;
-	}
+		frameStarts.points.frame4 = $('.frame-4').offset().top - 400;		
+		frameStarts.points.frame5Form = $('.frame-5 .frame-5_form-block').offset().top -h;	}
 }
 
 function scroller(){
@@ -48,5 +48,8 @@ function scroller(){
 	}
 	if(top >= frameStarts.points.frame4 && !$('.frame-4').hasClass('animate')){
 		$('.frame-4').addClass('animate');
+	}
+	if(top >= frameStarts.points.frame5Form && !$('.frame-5 .frame-5_form-block').hasClass('animate')){
+		$('.frame-5 .frame-5_form-block').addClass('animate');
 	}
 }
