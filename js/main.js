78af7e05478cdfa13156c759687dd8c3704d066b
Scooter)Ok <scooterok@gmail.com>
2017-09-26 23:02:50 +0300

    up

--- js/main.js
+++ js/main.js
@@ -1,7 +1,29 @@
+var clock;
 $(document).ready(function() {
 	//$.scrollSpeed(200, 800);
-	//$('.frame-5').addClass('animate');
+	//$('.frame-5 .frame-5_mac-block .frame-5_lines').addClass('animate');
+	//$('.frame-5 .frame-5_mac-block .wrapper .mac-text').addClass('animate');
+	//$('.frame-5 .frame-5_mac-block .wrapper .macbook').addClass('animate');
+	
+	
 	//$('.frame-2 ul').addClass('animate');
+
+
+	clock = $('#timer').FlipClock({
+		        clockFace: 'DailyCounter',
+		        autoStart: false,
+		        language : 'ru',
+		        //showSeconds: false,
+		        callbacks: {
+		        	stop: function() {
+		        		$('.message').html('The clock has stopped!')
+		        	}
+		        }
+		    });
+				    
+		    clock.setTime(220880);
+		    clock.setCountdown(true);
+		    clock.start();
 	frameStarts.init();
 	scroller();
 	$(document).scroll(function(e){
@@ -22,7 +44,12 @@ var frameStarts = {
 		frameStarts.points.ul3 = $('.frame-2 .ul-3').offset().top -750;
 		frameStarts.points.frame3 = $('.frame-3').offset().top -h;
 		frameStarts.points.frame4 = $('.frame-4').offset().top - 400;		
-		frameStarts.points.frame5Form = $('.frame-5 .frame-5_form-block').offset().top -h;	}
+		frameStarts.points.frame5Form = $('.frame-5 .frame-5_form-block').offset().top - 300;
+		frameStarts.points.frame5macText = $('.frame-5 .frame-5_mac-block .wrapper .mac-text').offset().top - 500;
+		frameStarts.points.frame5macLines = $('.frame-5 .frame-5_mac-block .frame-5_lines').offset().top - 250;
+		frameStarts.points.frame8 = $('.frame-8').offset().top - 300;		
+	}
+		
 }
 
 function scroller(){
@@ -51,5 +78,15 @@ function scroller(){
 	}
 	if(top >= frameStarts.points.frame5Form && !$('.frame-5 .frame-5_form-block').hasClass('animate')){
 		$('.frame-5 .frame-5_form-block').addClass('animate');
+	}
+	if(top >= frameStarts.points.frame5macText && !$('.frame-5 .frame-5_mac-block .wrapper .mac-text').hasClass('animate')){
+		$('.frame-5 .frame-5_mac-block .wrapper .mac-text').addClass('animate');
+	}
+	if(top >= frameStarts.points.frame5macLines && !$('.frame-5 .frame-5_mac-block .frame-5_lines').hasClass('animate') && !$('.frame-5 .frame-5_mac-block .wrapper .macbook').hasClass('animate')){
+		$('.frame-5 .frame-5_mac-block .frame-5_lines').addClass('animate');
+		$('.frame-5 .frame-5_mac-block .wrapper .macbook').addClass('animate');
+	}
+	if(top >= frameStarts.points.frame8 && !$('.frame-8').hasClass('animate')){
+		$('.frame-8').addClass('animate');
 	}
 }
